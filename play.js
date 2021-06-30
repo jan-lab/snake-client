c74cec5d329cc14d18696596ef32aa88810920f1
jan-lab <tnlatte@outlook.com>
2021-06-30 15:46:11 -0700

    created event handler that exits the game when the user input is ctrl+C

--- play.js
+++ play.js
@@ -1,2 +1,34 @@
 //const connect = require('./client');
 const {net, connect} = require("./client");
+
+
+const handleUserInput = function (key) {
+  // your code here
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // const handleUserInput = function (key) {
+  //   // your code here
+  //   // \u0003 maps to ctrl+c input
+  //   if (key === '\u0003') {
+  //     process.exit();
+  //   }
+  // };
+
+  stdin.on("data", handleUserInput);
+  
+  return stdin;
+
+};
+
+setupInput();
